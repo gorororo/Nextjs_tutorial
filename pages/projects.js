b68abe6d6c6a7ea21c668fd4f10561c7f375a730
gorororo <gorororo87@gmail.com>
2022-06-21 22:24:19 +0900

    배포전

--- pages/projects.js
+++ pages/projects.js
@@ -1,19 +1,35 @@
 import Layout from "../components/layout";
 import Head from "next/head";
 import { TOKEN, DATABASE_ID } from "../config";
+import ProjectItem from "../components/projects/projct-item";
+
+
 
 export default function Projects({projects}) {
   
   console.log(projects);
 
   return (
+    
+    
     <Layout>
-      <Head>
-        <title>락이 포트폴리오</title>
-        <meta name="description" content="차곡차곡 쌓아보자" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <h1>총 프로젝트 : {projects.length} </h1>
+      <div className="flex flex-col items-center justify-center min-h screen mb-10 px-6">
+        <Head>
+          <title>락이 포트폴리오</title>
+          <meta name="description" content="차곡차곡 쌓아보자" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <h1 className="text-4xl font-bold sm:text-6xl">
+          총 프로젝트 :  
+          <span className="pl-4 text-blue-500">{projects.results.length}</span>
+        </h1>
+
+        <div className="grid grid-cols-1 md:grid-cols-2 py-10 m-6 gap-8">
+          {projects.results.map((aProject) => (
+            <ProjectItem key={aProject.id} data={aProject}/>
+          ))}
+        </div>
+      </div>
     </Layout>
   );
 }
