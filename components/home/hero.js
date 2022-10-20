0c6bcc51d7ee2bce4ca89c245717c8a6ac6d2d2e
gorororo <gorororo87@gmail.com>
2022-10-20 15:43:46 +0900

    문구재변경

--- components/home/hero.js
+++ components/home/hero.js
@@ -4,8 +4,8 @@ import Link from "next/link";
 export default function Hero() {
     return (
         <>
-            <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-                <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
+            <div className="flex flex-col items-center mb-16 text-center lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 md:items-start md:text-left md:mb-0">
+                <h1 className="mb-4 text-3xl font-medium text-gray-900 title-font sm:text-4xl">
                     안녕하세요 프론트엔드 최광락 입니다.
                     <br className="hidden lg:inline-block" />
                     오늘도 열심히!
@@ -21,11 +21,6 @@ export default function Hero() {
                     도와주었냐 하는 것이다. (Jeff Atwood, StackOverFlow의
                     창립자)
                     <br className="hidden lg:inline-block" />
-                    우리 컴퓨터 프로그램의 슬픈 상태를 고려해보면
-                    소프트웨어 개발은 아직은 어둠의 마법이다. 아직은
-                    엔지니어링an engineering discipline이라고 부르기는 어렵다.
-                    (Bill Clinton, 미국 대통령)
-                    <br className="hidden lg:inline-block" />
                 </p>
                 <div className="flex justify-center">
                     <Link href="/projects">
@@ -33,7 +28,7 @@ export default function Hero() {
                     </Link>
                 </div>
             </div>
-            <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+            <div className="w-5/6 lg:max-w-lg lg:w-full md:w-1/2">
                 <Animation />
             </div>
         </>
